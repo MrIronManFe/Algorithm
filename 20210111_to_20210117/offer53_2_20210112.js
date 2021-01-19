aefd08f2d700b5bedc4b0ae39c03b6eb147735d3
ken <298074606@qq.com>
2021-01-18 18:38:43 -0800

    20210119_1040

--- 20210111_to_20210117/offer53_2_20210112.js
+++ 20210111_to_20210117/offer53_2_20210112.js
@@ -17,12 +17,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function (nums) {
-    let left = 0, right = nums.length - 1
+var missingNumber = function(nums) {
+    let left = 0,
+        right = nums.length - 1
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
         // console.log(mid);
-        (nums[mid] == mid) ? left = mid + 1 : right = mid - 1
+        (nums[mid] == mid) ? left = mid + 1: right = mid - 1
     }
     return left
 };
