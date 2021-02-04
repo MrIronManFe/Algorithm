00aa1cbc1dd5cb35a1c0149313756df7f2aacc2a
ken <298074606@qq.com>
2021-02-04 02:00:55 -0800

    20210204_1800

---
+++ 20210201_to_20210207/offer31_20210204.js
@@ -0,0 +1,45 @@
+/*
+输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如，序列 {1,2,3,4,5} 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列。
+
+示例 1：
+输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+输出：true
+解释：我们可以按以下顺序执行：
+push(1), push(2), push(3), push(4), pop() -> 4,
+push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+
+示例 2：
+输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+输出：false
+解释：1 不能在 2 之前弹出。
+
+
+提示：
+0 <= pushed.length == popped.length <= 1000
+0 <= pushed[i], popped[i] < 1000
+pushed 是 popped 的排列。
+*/
+
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function(pushed, popped) {
+    let stack = [],
+        point = 0
+    for (let index = 0; index < pushed.length; index++) {
+        let element = pushed[index]
+            // 入栈
+        stack.push(element)
+        console.log(stack)
+            // 循环出栈
+        while (stack.length > 0 && stack[stack.length - 1] === popped[point]) {
+            // console.log(popped[point])
+            // console.log(stack[stack.length - 1])
+            stack.pop()
+            point += 1
+        }
+    }
+    return stack.length === 0
+};
